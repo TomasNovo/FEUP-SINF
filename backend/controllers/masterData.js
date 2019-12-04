69c2e8c9eccf147c5a79baef05ddb6026fa08040
joanasmramos <joanasmramos98@gmail.com>
2019-12-04 16:02:46 +0000

    feat: add update operation to MasterData and refactor

--- backend/controllers/masterData.js
+++ backend/controllers/masterData.js
@@ -2,6 +2,14 @@
 
 const MasterData = require('../database/models/masterData');
 
+// Helper method
+function findMapping(id, thenCallback, catchCallback) {
+  MasterData.find().or([{idA: id}, {idB: id}]).orFail()
+  .limit(1)
+  .then(thenCallback)
+  .catch(catchCallback)
+}
+
 function create(req, res) {
   const { idA, idB } = req.body;
   const newMapping = new MasterData({
@@ -10,7 +18,7 @@ function create(req, res) {
   });
   newMapping.save()
   .then(mapping => {
-    res.status(201).json({mapping});
+    res.status(201).json(mapping);
   })
   .catch(err => {
     res.status(400).send(err);
@@ -20,21 +28,38 @@ function create(req, res) {
 function read(req, res) {
   const { id } = req.params;
 
-  MasterData.find().or([{idA: id}, {idB: id}]).orFail()
-  .then(mapping => {
-    res.status(200).json({mapping});
+  findMapping(id, mapping => {
+    res.status(200).json(mapping[0]);
+  }, err => {
+    res.status(404).send(err);
+  });
+}
+
+function readAll(req, res) {
+  MasterData.find()
+  .then(mappings => {
+    res.status(200).json(mappings);
   })
   .catch(err => {
     res.status(404).send(err);
   })
 }
 
-function readAll(req, res) {
-  res.send('Hello from readAll');
-}
-
 function update(req, res) {
-  res.send('Hello from update');
+  const { idA, idB } = req.body;
+  const { id } = req.params;
+  
+  findMapping(id, mapping => {
+    mapping[0].updateOne({ idA, idB })
+    .then(updated => {
+      res.status(200).send(updated);
+    })
+    .catch(err => {
+      res.status(404).send(err);
+    })
+  }, err => {
+    res.status(404).send(err);
+  });
 }
 
 function remove(req, res) {
