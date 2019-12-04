36612c3786136b0287ef0674dca77d141274fd21
joanasmramos <joanasmramos98@gmail.com>
2019-12-04 16:22:42 +0000

    feat: add delete operation for MasterData

--- backend/controllers/masterData.js
+++ backend/controllers/masterData.js
@@ -2,14 +2,6 @@
 
 const MasterData = require('../database/models/masterData');
 
-// Helper method
-function findMapping(id, thenCallback, catchCallback) {
-  MasterData.find().or([{idA: id}, {idB: id}]).orFail()
-  .limit(1)
-  .then(thenCallback)
-  .catch(catchCallback)
-}
-
 function create(req, res) {
   const { idA, idB } = req.body;
   const newMapping = new MasterData({
@@ -28,11 +20,13 @@ function create(req, res) {
 function read(req, res) {
   const { id } = req.params;
 
-  findMapping(id, mapping => {
-    res.status(200).json(mapping[0]);
-  }, err => {
+  MasterData.findOne({$or:[{ idA: id }, { idB: id }]}).orFail()
+  .then(mapping => {
+    res.status(200).json(mapping);
+  })
+  .catch(err => {
     res.status(404).send(err);
-  });
+  })
 }
 
 function readAll(req, res) {
@@ -49,21 +43,25 @@ function update(req, res) {
   const { idA, idB } = req.body;
   const { id } = req.params;
   
-  findMapping(id, mapping => {
-    mapping[0].updateOne({ idA, idB })
-    .then(updated => {
-      res.status(200).send(updated);
-    })
-    .catch(err => {
-      res.status(404).send(err);
-    })
-  }, err => {
+  MasterData.updateOne({$or:[{ idA: id }, { idB: id }]}, { idA, idB }).orFail()
+  .then(info => {
+    res.status(200).send(info);
+  })
+  .catch(err => {
     res.status(404).send(err);
-  });
+  })
 }
 
 function remove(req, res) {
-  res.send('Hello from remove');
+  const { id } = req.params;
+
+  MasterData.deleteOne({$or:[{ idA: id }, { idB: id }]}).orFail()
+  .then(info => {
+    res.status(200).send(info);
+  })
+  .catch(err => {
+    res.status(404).send(err);
+  })
 }
 
 module.exports = {
