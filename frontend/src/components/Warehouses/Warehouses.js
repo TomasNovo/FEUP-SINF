18c9b70b4af29ca94bca45710e0b181b34ac1236
ReleaseTheKraken <tomasnovo98@gmail.com>
2019-12-04 12:25:09 +0000

    Fixes on settings on warehouses

--- frontend/src/components/Warehouses/Warehouses.js
+++ frontend/src/components/Warehouses/Warehouses.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from "react-bootstrap/Container";
-import './Warehouses.scss'
+import './Warehouses.css'
 import PageTemplate from '../PageTemplate/PageTemplate';
 import localization from '../../assets/localization.png';
 import graphic from '../../assets/distribution.png';
@@ -11,22 +11,23 @@ class Warehouses extends React.Component
 	render() {
 		return (
                 <PageTemplate page="warehouses">
-                    <Container>
-                        <div className="all">
-                            <div className="left">
+                    <div className="all">
+                        <div className="left">
+                            <div className="warehouse-picker">
                                 <div className="btn-group">
-                                <div className="dropdown show">
-                                    <a className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        Dropdown link
-                                    </a>
-                                    <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                        <a className="dropdown-item" href="#">Action</a>
-                                        <a className="dropdown-item" href="#">Another action</a>
-                                        <a className="dropdown-item" href="#">Something else here</a>
-                                    </div>
+                                    <div className="dropdown show">
+                                        <a className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Warehouse 1
+                                        </a>
+                                        <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                            <a className="dropdown-item" href="./">Warehouse 2</a>
+                                            <a className="dropdown-item" href="./">Warehouse 3</a>
+                                            <a className="dropdown-item" href="./">Warehouse 4</a>
+                                        </div>
                                     </div>
                                 </div>
                                 <h1>Company A</h1>
+                            </div>
                                 <div className="localization">
                                     <img src={localization} alt="Localization" width="40" height="41" />
                                     <p>Rua Aval de Baixo nº158</p>
@@ -117,7 +118,6 @@ class Warehouses extends React.Component
 
                             </div>
                         </div>
-                    </Container>
                 </PageTemplate>
         );
 	}
