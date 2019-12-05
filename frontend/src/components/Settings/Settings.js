c96c676274988d5e3efd6a0c4adc14527dd036ba
Eduardo Silva <eduardo__lps@hotmail.com>
2019-12-05 01:36:20 +0000

    style: change settings style

--- frontend/src/components/Settings/Settings.js
+++ frontend/src/components/Settings/Settings.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 import './Settings.css'
 import PageTemplate from '../PageTemplate/PageTemplate';
 
@@ -9,60 +8,30 @@ class Settings extends React.Component
 	render() {
 		return (
             <PageTemplate page="settings">
-                    <div className="accountInfo">
-                        <p>Account Info</p>
-                        <div className="grant">
-                            {/* <p>Grant_Type */}
-                            <input type="text" placeholder="Grant Type"/>
-                            {/* </p> */}
-                        </div>
-                        <div className="clientId">
-                            {/* <p>Client_ID */}
-                            <input type="text" placeholder="Client ID"/>
-                            {/* </p> */}
-                        </div>
-                        <div className="clientSecret">
-                            {/* <p>Client_Secret */}
-                            <input type="password" placeholder="Client Secret"/>
-                            {/* </p> */}
-                        </div>
+                <div id="settings-wrapper">
+                    <div id="accountInfo">
+                        <h1>Account Info</h1>
+                        <input id="grant" type="text" placeholder="Grant Type"/>
+                        <input id="clientId" type="text" placeholder="Client ID"/>
+                        <input id="clientSecret" type="password" placeholder="Client Secret"/>
                     </div>
-                    <p className="accounting">Intercompany Accounting</p>
-                    <div className="Intercompany">
-                        <div className="Originating">
-                            <h1>Originating Company</h1>
-                            <br/>
-                            {/* <p>Company Name: </p> */}
-                            <input type="text" placeholder="Company Name"/>
-                            <br/>
-                            <div className="Debit">
-                                {/* <p>Debit Account: </p> */}
-                                <input type="text" placeholder="Debit Account"/>
-                            </div>
-                            <div className="Credit">
-                                {/* <p>Credit Account: </p> */}
-                                <input type="text" placeholder="Credit Account"/>
-                            </div>
-                        </div>
-                        <div className="Destination">
-                            <h1>Destination Company</h1>
-                            <br/>
-                            {/* <p>Company Name: </p> */}
-                            <input type="text" placeholder="Company Name"/>
-                            <br/>
-                            <div className="Debit">
-                                {/* <p>Debit Account: </p> */}
-                                <input type="text" placeholder="Debit Account"/>
-                            </div>
-                            <div className="Credit">
-                                {/* <p>Credit Account: </p> */}
-                                <input type="text" placeholder="Credit Account"/>
-                            </div>
-                        </div>
+                    <div id="originating">
+                        <h1>Originating Company</h1>
+                        <input className="company-name" type="text" placeholder="Company Name"/>
+                        <input className="debit" type="text" placeholder="Debit Account"/>
+                        <input className="credit" type="text" placeholder="Credit Account"/>
                     </div>
-                    <div className="Update">
-                        <input type="submit" value="Update"/>
+                    <div id="destination">
+                        <h1>Destination Company</h1>
+                        <input className="company-name" type="text" placeholder="Company Name"/>
+                        <input className="debit" type="text" placeholder="Debit Account"/>
+                        <input className="credit" type="text" placeholder="Credit Account"/>
                     </div>
+                    <div class="break"></div>
+                    <Button type="submit" id="update" variant="danger">
+                        Update
+                    </Button>
+                </div>
             </PageTemplate>   
         );
     }
