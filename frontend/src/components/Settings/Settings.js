f0f81419b7d7c03797f2382de35579211f638dc3
Joao Alves <joao-carlos.alves@hotmail.com>
2019-12-10 00:26:47 +0000

    feat: Fills accounts info

--- frontend/src/components/Settings/Settings.js
+++ frontend/src/components/Settings/Settings.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from "react-bootstrap/Button";
 import './Settings.css'
 import PageTemplate from '../PageTemplate/PageTemplate';
+import axios from 'axios';
 
 class Settings extends React.Component 
 {
@@ -9,25 +10,21 @@ class Settings extends React.Component
 		return (
             <PageTemplate page="settings">
                 <div id="settings-wrapper">
-                    <div id="accountInfo">
-                        <h1>Account Info</h1>
-                        <input id="grant" type="text" placeholder="Grant Type"/>
+                    <div id="accountInfo1">
+                        <h1>Account Info 1</h1>
+                        <label className="appidLabel">AppID</label>
                         <input id="clientId" type="text" placeholder="Client ID"/>
+                        <label className="appsecLabel">AppSecret</label>
                         <input id="clientSecret" type="password" placeholder="Client Secret"/>
                     </div>
-                    <div id="originating">
-                        <h1>Originating Company</h1>
-                        <input className="company-name" type="text" placeholder="Company Name"/>
-                        <input className="debit" type="text" placeholder="Debit Account"/>
-                        <input className="credit" type="text" placeholder="Credit Account"/>
+                    <div id="accountInfo2">
+                        <h1>Account Info 2</h1>
+                        <label className="appidLabel">AppID</label>
+                        <input id="clientId1" type="text" placeholder="Client ID"/>
+                        <label className="appsecLabel">AppSecret</label>
+                        <input id="clientSecret1" type="password" placeholder="Client Secret"/>
                     </div>
-                    <div id="destination">
-                        <h1>Destination Company</h1>
-                        <input className="company-name" type="text" placeholder="Company Name"/>
-                        <input className="debit" type="text" placeholder="Debit Account"/>
-                        <input className="credit" type="text" placeholder="Credit Account"/>
-                    </div>
-                    <div class="break"></div>
+                    <div className="break"></div>
                     <Button type="submit" id="update" variant="danger">
                         Update
                     </Button>
@@ -35,10 +32,40 @@ class Settings extends React.Component
             </PageTemplate>   
         );
     }
+
+    accountInfo1(){
+        axios.get('http://localhost:7000/api/company/0')
+        .then((response) => {
+            let appId = response.data[0].appId;
+            let clientSecret = response.data[0].appSecret;
+
+            document.getElementById('clientId').value=appId;
+            document.getElementById('clientSecret').value=clientSecret;
+        })
+        .catch((error) =>{
+            console.log('Error', error.message);
+        })
+    }
+
+    accountInfo2(){
+        axios.get('http://localhost:7000/api/company/1')
+        .then((response) => {
+            let appId = response.data[0].appId;
+            let clientSecret = response.data[0].appSecret;
+
+            document.getElementById('clientId1').value=appId;
+            document.getElementById('clientSecret1').value=clientSecret;
+        })
+        .catch((error) =>{
+            console.log('Error', error.message);
+        })
+    }
     
     componentDidMount()
     {
         document.title = "Settings";
+        this.accountInfo1();
+        this.accountInfo2();
     }
 };
 
