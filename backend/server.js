b2b0dcd2d78684d7ce023981cb07b0e14b1e059e
Eduardo Silva <eduardo__lps@hotmail.com>
2019-12-10 00:33:11 +0000

    feat: started work on processes process

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+const agenda = require('./agenda');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 var cors = require('cors');
@@ -28,3 +30,8 @@ const server = app.listen(7000, () =>
 {
 	console.log(`Express running → PORT ${server.address().port}`);
 });
+
+(async function() {
+	await agenda.start();
+	await agenda.every('2 seconds', 'update processes');
+})();
