83cb6cb3bad07ca65eab03531a81379ef8985bab
Joao Alves <joao-carlos.alves@hotmail.com>
2019-12-12 15:01:01 +0000

    feat: Update settings

--- frontend/src/components/Settings/Settings.js
+++ frontend/src/components/Settings/Settings.js
@@ -3,46 +3,78 @@ import Button from "react-bootstrap/Button";
 import './Settings.css'
 import PageTemplate from '../PageTemplate/PageTemplate';
 import axios from 'axios';
+import querystring from 'querystring';
 
 class Settings extends React.Component 
 {
 	render() {
 		return (
             <PageTemplate page="settings">
+                <form onSubmit={this.handleUpdate}>
                 <div id="settings-wrapper">
-                    <div id="accountInfo1">
-                        <h1>Account Info 1</h1>
-                        <label className="appidLabel">Name</label>
-                        <input id="name0" type="text" placeholder="name"/>
-                        <label className="appidLabel">AppID</label>
-                        <input id="clientId0" type="text" placeholder="Client ID"/>
-                        <label className="appsecLabel">AppSecret</label>
-                        <input id="clientSecret0" type="password" placeholder="Client Secret"/>
-                        <label className="appidLabel">Tenant</label>
-                        <input id="tenant0" type="text" placeholder="tenant"/>
-                        <label className="appidLabel">Organization</label>
-                        <input id="organization0" type="text" placeholder="organization"/>
-                    </div>
-                    <div id="accountInfo2">
-                        <h1>Account Info 2</h1>
-                        <label className="appidLabel">Name</label>
-                        <input id="name1" type="text" placeholder="name"/>
-                        <label className="appidLabel">AppID</label>
-                        <input id="clientId1" type="text" placeholder="Client ID"/>
-                        <label className="appsecLabel">AppSecret</label>
-                        <input id="clientSecret1" type="password" placeholder="Client Secret"/>
-                        <label className="appidLabel">Tenant</label>
-                        <input id="tenant1" type="text" placeholder="tenant"/>
-                        <label className="appidLabel">Organization</label>
-                        <input id="organization1" type="text" placeholder="organization"/>
-                    </div>
-                    <div className="break"></div>
-                    <Button type="submit" id="update" variant="danger">
-                        Update
-                    </Button>
+                        <div id="accountInfo1">
+                            <h1>Account Info 1</h1>
+                            <label className="appidLabel">Name</label>
+                            <input id="name0" type="text" placeholder="name"/>
+                            <label className="appidLabel">AppID</label>
+                            <input id="clientId0" type="text" placeholder="Client ID"/>
+                            <label className="appsecLabel">AppSecret</label>
+                            <input id="clientSecret0" type="password" placeholder="Client Secret"/>
+                            <label className="appidLabel">Tenant</label>
+                            <input id="tenant0" type="text" placeholder="tenant"/>
+                            <label className="appidLabel">Organization</label>
+                            <input id="organization0" type="text" placeholder="organization"/>
+                        </div>
+                        <div id="accountInfo2">
+                            <h1>Account Info 2</h1>
+                            <label className="appidLabel">Name</label>
+                            <input id="name1" type="text" placeholder="name"/>
+                            <label className="appidLabel">AppID</label>
+                            <input id="clientId1" type="text" placeholder="Client ID"/>
+                            <label className="appsecLabel">AppSecret</label>
+                            <input id="clientSecret1" type="password" placeholder="Client Secret"/>
+                            <label className="appidLabel">Tenant</label>
+                            <input id="tenant1" type="text" placeholder="tenant"/>
+                            <label className="appidLabel">Organization</label>
+                            <input id="organization1" type="text" placeholder="organization"/>
+                        </div>
+                        <div className="break"></div>
+                        <Button type="submit" id="update" variant="danger">
+                            Update
+                        </Button>
+                   
                 </div>
+                 </form>
             </PageTemplate>   
         );
+    }
+
+    handleUpdate(event){
+
+        for(let i = 0; i < 2; i++){
+
+            let name = event.target['name'+i].value;
+            let clientId = event.target['clientId'+i].value;
+            let clientSecret = event.target['clientSecret'+i].value;
+            let tenant = event.target['tenant'+i].value;
+            let organization = event.target['organization'+i].value;
+
+            axios.put('http://localhost:7000/api/company/' + i, querystring.stringify({
+                appId: clientId,
+                appSecret: clientSecret,
+                tenant: tenant,
+                organization: organization,
+                name: name
+            }))
+            .then((response) => {
+                console.log(response.status);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+        }
+
     }
 
     accountInfo(){
